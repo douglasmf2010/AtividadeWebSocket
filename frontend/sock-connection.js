afccb3c6c4c024910f4446ad627d995de8e4b4a9
GUILHERME FERNANDES DA SILVA <guilherme_silva177@estudante.sesisenai.org.br>
2026-05-20 17:04:21 -0300

    AHAM

--- frontend/sock-connection.js
+++ frontend/sock-connection.js
@@ -1,115 +1,139 @@
 let server = null;
-let currentUser = "";
+let currentUser = '';
 
-const user = document.getElementById("user");
-const telefone = document.getElementById("telefone");
-const senha = document.getElementById("senha");
+const user = document.getElementById('user');
+const telefone = document.getElementById('telefone');
+const senha = document.getElementById('senha');
 
 function cadastrar() {
+  if (server && server.readyState === WebSocket.OPEN) {
+    currentUser = user.value;
 
-    if(server && server.readyState === WebSocket.OPEN){
+    let obj = {
+      action: 'register',
+      usuario: user.value,
+      telefone: telefone.value,
+      senha: senha.value,
+    };
 
-        currentUser = user.value;
+    server.send(JSON.stringify(obj));
 
-        let obj = {
-            action: 'register',
-            usuario: user.value,
-            telefone: telefone.value,
-            senha: senha.value,
-        };
-
-        server.send(JSON.stringify(obj));
-
-        user.value = currentUser;
-    }
+    user.value = currentUser;
+  } else {
+    alert('ERRO você não esta conectado');
+  }
 }
 
 function logar() {
+//   serverConnect();
+  if (server && server.readyState === WebSocket.OPEN) {
+    currentUser = user.value;
 
-    if(server && server.readyState === WebSocket.OPEN){
-
-        currentUser = user.value;
-
-        let obj = {
-            action: 'logar',
-            usuario: document.getElementById("user-login").value,
-            senha: document.getElementById("senha-login").value
-        };
-
-        server.send(JSON.stringify(obj));
-
-        user.value = currentUser;
-    }
-}
-
-function serverConnect(){
-
-    server = new WebSocket("ws://127.0.0.1:3001");
-
-    server.onopen = () => {
-        connect.classList.add("hidden");
-        // disconnect.classList.remove("hidden");
+    let obj = {
+      action: 'logar',
+      usuario: document.getElementById('user-login').value,
+      senha: document.getElementById('senha-login').value,
     };
 
-    server.onclose = () => {
-        connect.classList.remove("hidden");
-        // disconnect.classList.add("hidden");
+    server.send(JSON.stringify(obj));
+
+    user.value = currentUser;
+  } else {
+    alert('ERRO você não esta conectado');
+  }
+}
+
+function criarSala() {
+  let nomesala = prompt('Qual o nome da sala');
+
+  if (server && server.readyState === WebSocket.OPEN) {
+    currentUser = user.value;
+
+    let obj = {
+      action: 'criarsalas',
+      nome: nomesala,
     };
 
-    server.onmessage = (sock) => {
+    server.send(JSON.stringify(obj));
+  } else {
+    alert('ERRO você não esta conectado');
+  }
+}
+function entrarSala(nomesala) {}
+// ";
+function serverConnect() {
+  server = new WebSocket('ws://127.0.0.1:3001');
 
-        let data = JSON.parse(sock.data);
+  server.onopen = () => {
+    connect.classList.add('hidden');
+    // disconnect.classList.remove("hidden");
+  };
 
-        if(data.action == "Who_is") {
+  server.onclose = () => {
+    connect.classList.remove('hidden');
+    // disconnect.classList.add("hidden");
+  };
 
-            let json = {
-                user: user.value,
-                action: 'confirm_connection',
-            };
+  server.onmessage = (sock) => {
+    let data = JSON.parse(sock.data);
+    console.log(data);
 
-            server.send(JSON.stringify(json));
+    if (data.action == 'Who_is') {
+      let json = {
+        user: user.value,
+        action: 'confirm_connection',
+      };
 
-       } 
+      server.send(JSON.stringify(json));
+    } else if (data.action == 'atualizarsala') {
+      let salaslist = document.getElementById('salas-list');
+      salaslist.innerHTML = '';
+      for (let i = 0; i < data.salas.length; i++) {
+        salaslist.innerHTML += `
+                <button type="button" class="salas" onclick = "entrarSala('${data.salas[i]}')>${data.salas[i]}</button>`;
+      }
     }
-    // else if(data.action == 'message') {
+  };
+  // else if(data.action == 'message') {
 
-    //         chatBox.insertAdjacentHTML(
-    //             "beforeend",
-    //             `<div class="received">
-    //                 <small>
-    //                     <label>${data.sender}</label>
-    //                 </small>
-    //                 <p>${data.message}</p>
-    //             </div>`
-    //         );
-    //     }
-    // };
+  //         chatBox.insertAdjacentHTML(
+  //             "beforeend",
+  //             `<div class="received">
+  //                 <small>
+  //                     <label>${data.sender}</label>
+  //                 </small>
+  //                 <p>${data.message}</p>
+  //             </div>`
+  //         );
+  //     }
+  // };
+  //   salaslist.innerHTML += `
+  //   <button type="button" class="salas">${nomesala}</button>`;
+  //     }
 }
 
-function serverDisconnect(){
+function serverDisconnect() {
+  if (server) {
+    server.send(
+      JSON.stringify({
+        action: 'disconnect',
+        user: user.value,
+      }),
+    );
 
-    if(server){
-
-        server.send(JSON.stringify({
-            action: 'disconnect',
-            user: user.value
-        }));
-
-        server.close();
-    }
+    server.close();
+  }
 }
 
-function serverSend(){
+function serverSend() {
+  if (server && server.readyState === WebSocket.OPEN) {
+    let obj = {
+      action: 'message',
+      sender: user.value,
+      receiver: receiver.value,
+      message: message.value,
+    };
 
-    if(server && server.readyState === WebSocket.OPEN){
-
-        let obj = {
-            action: 'message',
-            sender: user.value,
-            receiver: receiver.value,
-            message: message.value
-        };
-
-        server.send(JSON.stringify(obj));
-    }
+    server.send(JSON.stringify(obj));
+  }
 }
--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const ws = require('ws');
 const Usuario = require('./src/models/user');
 const Sala = require('./src/models/sala');
 
-
 let server = new ws.Server({ port: 3001 });
 
 let users = {};
+let salas = {};
+Sala.findAll({}).then((data) => {
+  for (let i = 0; i < data.length; i++) {
+    salas[data[i].nome] = [];
+  }
+});
 
 server.on('connection', (client) => {
   client.on('close', () => {});
@@ -51,7 +56,7 @@ server.on('connection', (client) => {
             message: 'Usuário já existe',
           }),
         );
-        console.log("\x1b[31m Usuário já existe :( \x1b[0m");
+        console.log('\x1b[31m Usuário já existe :( \x1b[0m');
       } else {
         let user = await Usuario.create({
           usuario: usuario,
@@ -62,22 +67,21 @@ server.on('connection', (client) => {
       }
     } else if (msgObject.action == 'logar') {
       const { usuario, senha } = msgObject;
-      console.log(usuario)
-      console.log(senha)
+      console.log(usuario);
+      console.log(senha);
       const user = await Usuario.findOne({
         where: {
           usuario: usuario,
           senha: senha,
         },
       });
-      
-      console.log("se tiver vivo diga \x1b[32m\"oi\"\x1b[0m");
+
+      console.log('se tiver vivo diga \x1b[32m"oi"\x1b[0m');
       console.log(user);
-      
+
       if (user) {
-        
         users[usuario] = client;
-        
+
         client.send(
           JSON.stringify({
             action: 'logar',
@@ -89,6 +93,16 @@ server.on('connection', (client) => {
         );
 
         console.log(`Usuário ${usuario} logado`);
+        let salastemp = [];
+        for (const key in salas) {
+          salastemp.push(key);
+        }
+        client.send(
+          JSON.stringify({
+            action: 'atualizarsala',
+            salas: salastemp,
+          }),
+        );
       } else {
         client.send(
           JSON.stringify({
@@ -97,6 +111,42 @@ server.on('connection', (client) => {
             message: 'Usuário ou senha inválidos',
           }),
         );
+      }
+    } else if (msgObject.action == 'criarsalas') {
+      const { nome } = msgObject;
+
+      const sala = await Sala.findOne({
+        where: {
+          nome: nome,
+        },
+      });
+      if (sala) {
+        client.send(
+          JSON.stringify({
+            action: 'criarsalas',
+            success: false,
+            message: 'Sala já existe',
+          }),
+        );
+        console.log('\x1b[31m A Sala já existe :( \x1b[0m');
+      } else {
+        let sala = await Sala.create({
+          nome: nome,
+        });
+        await sala.save();
+        salas[sala.nome] = [];
+        let salastemp = [];
+        for (const key in salas) {
+          salastemp.push(key);
+        }
+        for (const username in users) {
+          users[username].send(
+            JSON.stringify({
+              action: 'atualizarsala',
+              salas: salastemp,
+            }),
+          );
+        }
       }
     }
   });
